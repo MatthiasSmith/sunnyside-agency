792cc3f4d92db4c6efca7daae3a0bb6185800274
Matthew Smith <musicsmith4@gmail.com>
2021-07-16 17:00:54 +0200

    set up project to use next.js and tailwind css

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Frontend Mentor | Sunnyside agency landing page</title>
+        <link rel='icon' href='/images/favicon-32x32.png' />
+      </Head>
+      <main>
+        <h1>hello</h1>
+      </main>
+    </>
+  );
+}
