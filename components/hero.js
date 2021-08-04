107dfd5de30777c64f195f9203f01e2bcee48fdb
Matthew Smith <musicsmith4@gmail.com>
2021-08-04 14:54:30 +0200

    style hero section to look good while image loads

--- components/hero.js
+++ components/hero.js
@@ -4,7 +4,7 @@ import ResponsiveImage from './responsive-image';
 
 const Hero = () => {
   return (
-    <section className='-mt-24 relative z-0'>
+    <section className='hero-section bg-blueBg -mt-24 relative z-0'>
       <div className='absolute w-full top-36 px-6 left-1/2 -translate-x-1/2 sm:top-28 md:top-36 lg:top-40'>
         <h1 className='text-h1 lg:text-h1Lg tracking-1/4-rem font-serif uppercase text-center text-white opacity-0 motion-safe:animate-fade-slide-down'>
           We are creatives
@@ -30,6 +30,16 @@ const Hero = () => {
         @media (prefers-reduced-motion: no-preference) {
           .down-arrow {
             animation-delay: 400ms;
+          }
+
+          .hero-section {
+            min-height: calc(100vw / 375 * 538);
+          }
+
+          @media screen and (min-width: 40em) {
+            .hero-section {
+              min-height: calc(100vw / 9 * 5);
+            }
           }
         }
       `}</style>
